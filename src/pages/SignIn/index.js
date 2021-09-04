206e1eded62edb299e33355bdce185792c18523c
Luiz Felipe M. Pereira <luiz_mst4@hotmail.com>
2021-09-04 00:12:57 -0300

    refactor: verify is loading

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -9,7 +9,7 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn, loadingAuth } = useContext(AuthContext);
 
   function handleSubmit(e){
     e.preventDefault();
@@ -31,7 +31,7 @@ function SignIn() {
           <h1>Entrar</h1>
           <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
           <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value) } />
-          <button type="submit">Acessar</button>
+          <button type="submit">{loadingAuth ? 'Carregando...' : 'Acessar'}</button>
         </form>  
 
         <Link to="/register">Criar uma conta</Link>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -8,7 +8,7 @@ function SignUp() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const {signUp} = useContext(AuthContext)
+  const {signUp, loadingAuth} = useContext(AuthContext)
 
   function handleSubmit(e){
     e.preventDefault();
@@ -31,7 +31,7 @@ function SignUp() {
           <input type="text" placeholder="Your name" value={nome} onChange={ (e) => setNome(e.target.value) }/>
           <input type="email" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
           <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value) } />
-          <button type="submit">Acessar</button>
+          <button type="submit">{loadingAuth ? 'Carregando...' : 'Acessar'}</button>
         </form>  
 
         <Link to="/">Já tem uma conta? Entre!</Link>
